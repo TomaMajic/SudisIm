9e5613692cc1de6aaf366b975f600c7ef9acda2c
jPaladin <josip.paladin@fer.hr>
2018-02-03 17:13:35 +0100

    added tests for city

--- SudisIm.Model/Models/City.cs
+++ SudisIm.Model/Models/City.cs
@@ -2,6 +2,15 @@
 {
     public class City
     {
+
+        public City()
+        {}
+        public City(string name, int postalCode)
+        {
+            this.Name = name;
+            this.PostalCode = postalCode;
+        }
+
         public virtual long Id { get; set; }
         public virtual string Name { get; set; }
         public int PostalCode { get; set; }
---
+++ SudisIm.Tests/Models/CityTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SudisIm.Model.Models;
+
+namespace SudisIm.Tests.Models
+{
+    [TestClass]
+    public class CityTests
+    {
+        [TestMethod]
+        public void CreateCityTest()
+        {
+            var cityName = "Split";
+            var postalCode = 21000;
+
+            var city = new City(cityName, postalCode);
+
+            Assert.IsTrue(city.Name == cityName, "city.Name == cityName");
+            Assert.IsTrue(city.PostalCode == postalCode, "city.PostalCode == postalCode");
+        }
+    }
+}
