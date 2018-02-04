7e142e994909745c080b4dfe8055ddf019a9cb91
jPaladin <josip.paladin@fer.hr>
2018-02-05 00:17:01 +0100

    referee calendar VM init

---
+++ SudisIm/Models/Calendar/RefereeCalendarViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SudisIm.Models.Calendar
+{
+    public class RefereeCalendarViewModel
+    {
+        public IEnumerable<Game> Type { get; set; }
+    }
+}
