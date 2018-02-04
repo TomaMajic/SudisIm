9e1cfbf21ff0cfa4ebd4c5585b6ddbbd29e6b0cc
jPaladin <josip.paladin@fer.hr>
2018-02-04 10:47:23 +0100

    updates game and ref models

--- SudisIm.DAL/Mappings/GameMap.cs
+++ SudisIm.DAL/Mappings/GameMap.cs
@@ -10,7 +10,9 @@ namespace SudisIm.DAL.Mappings
             Id(g => g.Id).GeneratedBy.Native();
             Map(g => g.Address);
             Map(g => g.StartTime);
+            Map(g => g.NoOfReferees);
             References(g => g.City);
+            References(g => g.MinimalLicence);
             References(g => g.HomeTeam, "HomeTeamId");
             References(g =>g.AwayTeam, "AwayTeamId");
             HasManyToMany(g => g.Referees).Cascade.SaveUpdate();
--- SudisIm.Model/Models/Referee.cs
+++ SudisIm.Model/Models/Referee.cs
@@ -18,6 +18,7 @@
         public virtual string Address { get; set; }
         public virtual bool IsDeleted { get; set; }
         public virtual string Description { get; set; }
+        public virtual string Contact { get; set; }
         public virtual Licence Licence { get; set; }
        
         public virtual City City { get; set; }
